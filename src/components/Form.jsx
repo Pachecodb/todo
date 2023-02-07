9e57126fe58f9f22591783bf3b583e77fbdbdc4f
Pachecodb <jeferson_luizz@hotmail.com>
2023-02-07 17:47:06 -0300

    Form.jsx
    salvando tarefa em localstorage como um objeto com ID, e esta listando tarefas.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,40 @@
-import React, {  useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Form() {
 
     const [tarefa, setTarefa] = useState("");
     const [lista, setLista] = useState([]);
 
-    useEffect(()=>{
-        if(localStorage.getItem("Lista_tarefas")){
+    useEffect(() => {
+        if (localStorage.getItem("Lista_tarefas")) {
             const listaSalva = JSON.parse(localStorage.getItem("Lista_tarefas"));
             setLista(listaSalva);
         }
-    },[])
+    }, []);
 
     const addTarefa = () => {
         if (tarefa.trim()) {
-          const novaTarefa = { title: tarefa };
-          setLista([...lista, novaTarefa]);
-          localStorage.setItem("Lista_tarefas", JSON.stringify([...lista, novaTarefa]));
-          setTarefa("");
+            const novaTarefa = { id: new Date().getTime().toString(), title: tarefa };
+            setLista([...lista, novaTarefa]);
+            localStorage.setItem("Lista_tarefas", JSON.stringify([...lista, novaTarefa]));
         }
-      };
+    };
 
+    const deleteTarefa = (tarefa) => {
+        console.log("exlcuido")
+    }
     return (
         <div>
-            <h3>Lista de tarefas</h3>
-            <input type="text" placeholder="Digite a tarefa" name='tarefa' id='id' onChange={(e) => setTarefa(e.target.value)} value={tarefa} autoFocus required></input>
-            <button type='submit' onClick={addTarefa}>Adicionar</button>
-            <h3>{tarefa}</h3>
+            <form>
+                <h3>Lista de tarefas</h3>
+                <input type="text" placeholder="Digite a tarefa" name='tarefa' onChange={(e) => setTarefa(e.target.value)} value={tarefa} required></input>
+                <button type='submit' onClick={addTarefa}>Adicionar</button>
+            </form>
+            {lista.map((tarefa) => (
+                <div key={tarefa.id}>
+                    <span>{tarefa.title}</span>
+                </div>
+            ))}
         </div>
     )
 }
