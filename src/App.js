bd3230f6766de7d4ae7b255bc6337663bebfe18b
Pachecodb <jeferson_luizz@hotmail.com>
2023-02-24 15:12:29 -0300

    app
    adicionando o componente footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Form from './components/Form';
+import Footer from './components/layout/Footer';
+import React from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <Form/>
-    </div>
-  );
-}
 
-export default App;
+
+  return (
+    <>
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default App
